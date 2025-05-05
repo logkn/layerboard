fc031a177c52ac9c8ada5fbeff25dc2e325af1c7
Logan Knapp <logkna@gmail.com>
2025-05-05 07:32:11 -0400

    cleared

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,10 @@
 import "./App.css";
-import { DiagramCanvas } from "./features/diagram/DiagramCanvas";
-import { PropertiesPanel } from "./components/PropertiesPanel";
-import { KeyboardShortcuts } from "./components/KeyboardShortcuts";
 
 function App() {
-  return (
-    <div style={{ display: "flex", height: "100vh" }}>
-      {/* Keyboard shortcuts handler (invisible component) */}
-      <KeyboardShortcuts />
-
-      <div style={{ flex: 1, position: "relative" }}>
-        <DiagramCanvas />
-      </div>
-      <PropertiesPanel />
-    </div>
-  );
+    return (
+        <div style={{ display: "flex", height: "100vh" }}>
+        </div>
+    );
 }
 
 export default App;
